7917bfe7320d356f54e46de82f0013a54c5337f2
happymalyo <malyo20.daiki@gmail.com>
2024-10-15 17:59:00 +0200

    add tailwindcss

--- src/App2.vue
+++ src/App2.vue
@@ -1,32 +1,54 @@
-<script>
-export default {
-  // Options API approach
-  data() {
-    return {
-      name: "Hello Mario",
-      status: "active",
-      tasks: ["eat", "sleep", "code"],
-      link: "https://www.google.com",
-    };
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === "active") {
-        this.status = "inactive";
-      } else {
-        this.status = "active";
-      }
-    },
-  },
+<script setup>
+import { ref, onMounted } from "vue"; // It's reactive like React useState
+
+// Composition API approach
+const name = ref("Hello Mario");
+const status = ref("active");
+const tasks = ref(["eat", "sleep", "code"]);
+const newTask = ref("");
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
 };
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    console.log(tasks);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  // You should bring this in from Vue
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+  tasks.value = data.map((item) => item.title);
+});
 </script>
 <template>
   <h1>{{ name }}</h1>
   <p v-if="status === 'active'">User active</p>
   <p v-else>User inactive</p>
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
   <ul>
-    <li v-for="item in tasks" :key="item">
-      <span>{{ item }}</span>
+    <li v-for="(item, index) in tasks" :key="item">
+      <span
+        >{{ item }}
+        <button @click="deleteTask(index)" style="cursor: pointer">
+          x
+        </button></span
+      >
     </li>
   </ul>
   <button @click="toggleStatus">Change status</button>
