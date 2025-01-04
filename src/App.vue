293739abdbda84e7296e8c5251a21a427077451b
happymalyo <malyo20.daiki@gmail.com>
2025-01-04 14:15:52 +0300

    feat: add og-image

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import Footer from "@/components/Footer.vue";
+import { useHead } from "@vueuse/head";
 import { RouterView } from "vue-router";
+import ogImage from "@/assets/images/og-image.jpg";
+
+const baseUrl = window.location.origin;
+const imageUrl = `${baseUrl}${ogImage}`;
+
+useHead({
+    meta: [
+        {
+            property: "og:image",
+            content: imageUrl,
+        },
+    ],
+});
 </script>
 
 <template>
